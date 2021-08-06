5acc5659577d0bd3f9a1ab28b9e12612a73f5953
Netra Kayastha <netrakayastha@nexsystech.com>
2021-08-06 16:26:10 -0400

    Add project files.

---
+++ Helpers/BasicAuthHandler.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BasicAuth.Helpers
+{
+    public class BasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Authorisation Headers");
+            }
+
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                string[] credentials = Encoding.UTF8.GetString(credentialBytes).Split(":");
+
+                string username = credentials[0];
+                string password = credentials[1];
+
+                if (username == "abc" && password == "abc")
+                {
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, "abc")
+                    };
+
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var token = new AuthenticationTicket(principal, Scheme.Name);
+
+                    return AuthenticateResult.Success(token);
+
+                }
+
+                else
+                {
+                    return AuthenticateResult.Fail("invalid credentials");
+                }
+
+            }
+
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorisation Header");
+            }
+        }
+    }
+}
